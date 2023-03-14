3ab0fd64776b0decb9dbc327b2bcb8ed674db96e
Juraj Ahel <juraj.ahel@gmail.com>
2023-03-14 23:55:52 +0100

    mmseqs work update

---
+++ MmseqsHelperLib/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace MmseqsHelperLib;
+
+public static class Constants
+{
+    public static readonly Dictionary<SupportedMmseqsModule,string> ModuleStrings = new Dictionary<SupportedMmseqsModule,string>()
+    {
+        {SupportedMmseqsModule.Search, "search" },
+        {SupportedMmseqsModule.ExpandAlignment, "expandaln" },
+        { SupportedMmseqsModule.Align, "align"},
+        {SupportedMmseqsModule.FilterResult, "filterresult" },
+        { SupportedMmseqsModule.ConvertResultToMsa , "result2msa"},
+        {SupportedMmseqsModule.LinkDatabase, "lndb" },
+        {SupportedMmseqsModule.MoveDatabase, "mvdb" },
+        {SupportedMmseqsModule.MergeDatabases, "mergedbs" }
+    };
+
+}
